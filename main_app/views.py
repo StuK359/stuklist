94baea2074df04a9a452dc43ccf9863d0156c70d
Stuart Klingman <stuartk@avatek.com>
2021-04-23 12:35:02 -0700

    Complete Step 3.1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
+from django.views.generic.edit import CreateView
 
 # Create your views here.
+def index(request):
+    todos = Todo.objects.all()
+    return render(request, 'index.html', { 'todos': todos })
+
+class TodoCreate(CreateView):
+    model = Todo
+    fields = "__all__"
+
+def delete_todo(request, todo_id):
+    Todo.objects.get(id=todo_id).delete()
+    return redirect("/")
